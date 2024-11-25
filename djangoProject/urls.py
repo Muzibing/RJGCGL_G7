c6da0dc6b27311001e86a48500ebe967cf6fe6bc
wen hao <3220104512@zju.edu.cn>
2024-11-25 19:54:25 +0800

    delete some strange files

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -8,5 +8,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('backend.urls')),  # 指向 backend 应用的 urls.py
+    path('deepseek/', include('backend.chat_urls')),
     path('', TemplateView.as_view(template_name='frontend/index.html')),
 ]
